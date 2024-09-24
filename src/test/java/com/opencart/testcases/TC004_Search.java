39ec3e23f851ee5baa54046166140710d0861581
Sopan <sopankutew@gmail.com>
2024-09-24 16:15:55 +0530

    deleted report | created 2 TC

---
+++ src/test/java/com/opencart/testcases/TC004_Search.java
@@ -0,0 +1,9 @@
+package com.opencart.testcases;
+
+import org.testng.annotations.Test;
+
+public class TC004_Search {
+  @Test
+  public void f() {
+  }
+}
---
+++ src/test/java/com/opencart/testcases/TC005_SearchItem.java
@@ -0,0 +1,5 @@
+package com.opencart.testcases;
+
+public class TC005_SearchItem {
+	
+}
